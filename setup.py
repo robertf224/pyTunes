80023919858f3f0b1a0e9e3673df814c5cf4c286
Robert Fidler <robertf224@yahoo.com>
2013-10-18 00:57:54 -0400

    added setup, readme

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'pyTunes',
+	version = '1.0',
+	description = 'A python scripting interface for iTunes',
+	author = 'Robert Fidler',
+	author_email = 'robertf224@gmail.com',
+	packages = find_packages(),
+	url = 'https://github.com/robertf224/pyTunes/',
+	keywords = ['iTunes', 'music', 'pyTunes', 'mac', 'osx']
+	)
